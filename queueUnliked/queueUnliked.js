2c1b77f9f1815692138b76f81bf238bd2cba8ac3
podpah <stream_bars07@icloud.com>
2025-07-06 23:02:03 +0100

    Expand to albums

--- queueUnliked/queueUnliked.js
+++ queueUnliked/queueUnliked.js
@@ -4,7 +4,7 @@
 // DESCRIPTION: Queues up songs that aren't liked in current playlist
 /// <reference path="../globals.d.ts" />
 
-function queueUnlikedMain() {
+(function queueUnlikedMain() {
 	if (!(Spicetify.CosmosAsync && Spicetify.ContextMenu)) {
 		setTimeout(queueUnlikedMain, 200);
 		return;
@@ -17,8 +17,10 @@ function queueUnlikedMain() {
 		const uri = uris[0];
 		const uriObj = Spicetify.URI.fromString(uri);
 		if (
+			uriObj.type === Spicetify.URI.Type.PLAYLIST_V2 ||
+			uriObj.type == Spicetify.URI.Type.ALBUM ||
 			uriObj.type === Spicetify.URI.Type.PLAYLIST ||
-			uriObj.type === Spicetify.URI.Type.PLAYLIST_V2
+			uriObj.type === Spicetify.URI.Type.DAILY_MIX
 		)
 			return true;
 		return false;
@@ -48,6 +50,9 @@ function queueUnlikedMain() {
 		if (unliked_songs.length > 0) {
 			await Spicetify.Platform.PlayerAPI.addToQueue(unliked_songs);
 		}
+        else {
+            Spicetify.showNotification("All songs in this playlist are liked.");
+        }
 	}
 
 	new Spicetify.ContextMenu.Item(
@@ -56,4 +61,4 @@ function queueUnlikedMain() {
 		uriPlaylist,
 		`<svg role="img" height="16" width="16" viewBox="0 0 16 16" fill="currentColor"><path d="M16 15H2v-1.5h14V15zm0-4.5H2V9h14v1.5zm-8.034-6A5.484 5.484 0 0 1 7.187 6H13.5a2.5 2.5 0 0 0 0-5H7.966c.159.474.255.978.278 1.5H13.5a1 1 0 1 1 0 2H7.966zM2 2V0h1.5v2h2v1.5h-2v2H2v-2H0V2h2z"></path></svg>`
 	).register();
-}
+})();
